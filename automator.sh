9e2ee46adad16fb9390b9d341017caba5d66bf34
Zeussssssss <adi.kr02@yahoo.in>
2023-05-25 20:24:43 +0530

    First Draft completed

--- automator.sh
+++ automator.sh
@@ -6,56 +6,61 @@ TEST_DATASET_NAME="scanner3DTop-2223-03-07__00-00-00-000_lettuce.tar.gz"
 NUMBER_OF_FILES=90
 
 while test $# -gt 0; do
-    case "$1" in
-        -h|--help)
-            echo "script usage: $(basename $0) [-p pathname] [-u username] [-t test_dataset_name(DON'T INCLUDE .tar.gz)] [-n number_of_files]"
-            echo "example: ./automator.sh -p /iplant/home/shared/phytooracle/season_16_sorghum_yr_2023/level_0/stereoTop/ToDelete_stereoTop-2223-05-08__00-00-00-000_sorghum.tar.gz -u adityakumar -n 10 -t subset"
-            echo "Check the github repo for more information."
-            exit 0
-            ;;
-        -u)
-            shift
-            USER=$1
-            shift
-            ;;
-        -p)
-            shift
-            SCAN_PATH=$1
-            shift
-            ;;
-        -t)
-            shift
-            TEST_DATASET_NAME=$1
-            shift
-            ;;
-        -n)
-            shift
-            NUMBER_OF_FILES=$1
-            shift
-            ;;
-        *)
-            echo "script usage: $(basename $0) [-p pathname] [-u username] [-t test_dataset_name(DON'T INCLUDE .tar.gz)] [-n number_of_files]" >&2
-            exit 1
-            ;;
-    esac
+  case "$1" in
+  -h | --help)
+    echo "script usage: $(basename $0) [-p pathname] [-u username] [-t test_dataset_name(DON'T INCLUDE .tar.gz)] [-n number_of_files]"
+    echo "example: ./automator.sh -p /iplant/home/shared/phytooracle/season_16_sorghum_yr_2023/level_0/stereoTop/stereoTop-2223-05-08__00-00-00-000_sorghum.tar.gz -u ak -n 10 -t subset"
+    echo "Check the github repo for more information."
+    exit 0
+    ;;
+  -u)
+    shift
+    USER=$1
+    shift
+    ;;
+  -p)
+    shift
+    SCAN_PATH=$1
+    shift
+    ;;
+  -t)
+    shift
+    TEST_DATASET_NAME=$1
+    shift
+    ;;
+  -n)
+    shift
+    NUMBER_OF_FILES=$1
+    shift
+    ;;
+  *)
+    echo "script usage: $(basename $0) [-p pathname] [-u username] [-t test_dataset_name(DON'T INCLUDE .tar.gz)] [-n number_of_files]" >&2
+    exit 1
+    ;;
+  esac
 done
 
 SCAN_FILE_NAME=$(echo ${SCAN_PATH} | cut -d '/' -f 9)
 SCAN_DIR_NAME=$(echo ${SCAN_FILE_NAME} | cut -d '.' -f 1)
 
-ssh filexfer "cd /xdisk/dukepauli/${USER}/;iget -KFT ${SCAN_PATH}"
-echo "Scan ${SCAN_FILE_NAME} download complete. (1/3)"
+ssh filexfer "cd /xdisk/dukepauli/${USER}/;iget -KT ${SCAN_PATH}"
+echo "Scan ${SCAN_FILE_NAME} download complete. (1/5)"
 
 tar -xzvf ${SCAN_FILE_NAME}
-echo "File extraction done. (2/3)"
+echo "File extraction done. (2/4)"
 
-# --TESTED TILL THIS POINT--
-echo "Selecting files for the test dataset. (3/3)"
+echo "Selecting files for the test dataset. (3/5)"
 mkdir ${TEST_DATASET_NAME}
 cd ${SCAN_DIR_NAME}
-FILE_COUNT=$(ls|wc -l)
-if [ $((FILE_COUNT%2)) -eq 0 ]; then
-  ls | head -n $(( ${FILE_COUNT} + 1)/2 + NUMBER_OF_FILES/2 ) | tail -n ${NUMBER_OF_FILES} | xargs -I {} cp {} ${TEST_DATASET_NAME}
+FILE_COUNT=$(ls | wc -l)
+if [ $((FILE_COUNT % 2)) -eq 0 ]; then
+  ls | head -n $((($FILE_COUNT + 1) / 2 + $NUMBER_OF_FILES / 2)) | tail -n ${NUMBER_OF_FILES} | xargs -I {} cp {} "${TEST_DATASET_NAME}/"
 else
-  ls | head -n $(( ${FILE_COUNT} + 1) / 2 + NUMBER_OF_FILES/2 ) | tail -n $((NUMBER_OF_FILES+1)) | xargs -I {} cp {} ${TEST_DATASET_NAME}
+  ls | head -n $((($FILE_COUNT + 1) / 2 + $NUMBER_OF_FILES / 2)) | tail -n $((NUMBER_OF_FILES + 1)) | xargs -I {} cp {} "${TEST_DATASET_NAME}/"
 fi
+
+echo "Zipping and uploading the test dataset. (4/5)"
+tar -czvf "${TEST_DATASET_NAME} .tar.gz " "${TEST_DATASET_NAME}/"
+ssh filexfer "cd /xdisk/dukepauli/${USER}/;iput -KT '$TEST_DATASET_NAME .tar.gz'"
+
+echo "Done (5/5)"
